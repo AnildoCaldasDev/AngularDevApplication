14aa28180fe3055b3f83500290d95403a84e8b55
Anildo Caldas <anildk5@hotmail.com>
2020-06-04 12:12:13 -0300

    created statics forms control/group examples.

--- src/app/views/implementacoes/reactiveforms/reactiveforms.component.ts
+++ src/app/views/implementacoes/reactiveforms/reactiveforms.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 // import { FormControl } from '@angular/forms';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 
 @Component({
@@ -10,16 +10,50 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class ReactiveformsComponent implements OnInit {
 
+  constructor(private formBuilder: FormBuilder) { }
+
+
   //examples with formControl:
   // name = new FormControl('');
   // updateName() {
   //   this.name.setValue('Anildo Caldas');
   // }
 
+  // profileForm = new FormGroup({
+  //   firstName: new FormControl(''),
+  //   lastName: new FormControl(''),
+  //   address: new FormGroup({
+  //     street: new FormControl(''),
+  //     number: new FormControl('')
+  //   })
+  // });
+
+  profileForm = this.formBuilder.group({
+    firstName: ['', Validators.required],
+    lastName: [''],
+    address: this.formBuilder.group({
+      street: [''],
+      number: [''],
+    }),
+  });
 
 
 
-  constructor() { }
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Anildo Caldas',
+      teste: 'dgdgdgd',
+      address: {
+        street: 'Rua do retiro, 40'
+      }
+    });
+
+  }
+
+
+  onSubmit() {
+    console.log(this.profileForm.value);
+  }
 
   ngOnInit(): void {
   }
