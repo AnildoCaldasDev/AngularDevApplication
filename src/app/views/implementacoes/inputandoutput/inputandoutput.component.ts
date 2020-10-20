2f8466eb666328c17345643716516033c48bee0f
anildk5@hotmail.com <anildk5@hotmail.com>
2020-10-19 22:09:12 -0300

    added areference to another component

--- src/app/views/implementacoes/inputandoutput/inputandoutput.component.ts
+++ src/app/views/implementacoes/inputandoutput/inputandoutput.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { BibliotecarxjsService } from '../../../services/bibliotecarxjs.service';
 
 @Component({
   selector: "app-inputandoutput",
   templateUrl: "./inputandoutput.component.html",
 })
 export class InputandoutputComponent implements OnInit {
-  constructor() {}
+  constructor(private _bblRjxs: BibliotecarxjsService) {}
   //URL: https://angular.io/guide/inputs-outputs
+
+  @ViewChild('idModelo') modeloPlanoId: ElementRef;
 
   items: string[] = ["Item 1", "Item 2"];
 
@@ -21,4 +24,10 @@ export class InputandoutputComponent implements OnInit {
     let indexItem = this.items.indexOf(itemToDelete);
     if (indexItem > -1) this.items.splice(indexItem, 1);
   }
+
+  atualizarValor(){
+    let valor: number = this.modeloPlanoId.nativeElement.value;
+    this._bblRjxs.atualizaModeloPlano(valor);
+  }
+
 }
